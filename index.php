a3f116987c280ba389027cc8d743d993a6f52157
Alvaro <alvaro_delgado_1994@hotmail.com>
2014-06-03 11:47:23 +0200

    Muestra 100 registros ordenados

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$db = dbase_open ('Goldmine DB/Contact2.DBF', 0);
+
+
+
+$num_reg = dbase_numrecords ($db);
+
+echo '<table border=1px solid black>';
+$i = 1;
+    $Record = dbase_get_record_with_names ($db, $i);
+
+    foreach ($Record as $key => $value){
+        echo '<td>'.$key.'</td>';
+
+    }
+
+for ($i =1; $i < 100; $i++) {
+    $Record = dbase_get_record_with_names ($db, $i);
+echo '<tr>';
+    foreach ($Record as $key => $value){
+        echo '<td>'.$value.'</td>';
+
+
+    }
+    echo '</tr>';
+}
+
+echo '</table>';
