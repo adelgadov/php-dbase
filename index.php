1475da0c6e26f7ead2618c71d6192ea858ebd56f
Alvaro <alvaro_delgado_1994@hotmail.com>
2014-06-03 11:57:53 +0200

    Busca una columna en concreto

--- index.php
+++ index.php
@@ -11,17 +11,21 @@ $i = 1;
     $Record = dbase_get_record_with_names ($db, $i);
 
     foreach ($Record as $key => $value){
+        if ($key == 'ACCOUNTNO') {
+
         echo '<td>'.$key.'</td>';
 
     }
-
+    }
 for ($i =1; $i < 100; $i++) {
     $Record = dbase_get_record_with_names ($db, $i);
 echo '<tr>';
     foreach ($Record as $key => $value){
-        echo '<td>'.$value.'</td>';
+       if ($key == 'ACCOUNTNO') {
 
+            echo '<td>'.$value.'</td>';
 
+       }
     }
     echo '</tr>';
 }
