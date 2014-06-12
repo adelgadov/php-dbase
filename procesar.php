87dc60fd760a56edb31f22f66ea6c94010c2773d
Alvaro <alvaro_delgado_1994@hotmail.com>
2014-06-12 18:43:35 +0200

    Removidos campos en blanco y estilizada la tabla

--- procesar.php
+++ procesar.php
@@ -7,19 +7,19 @@ $segundovalor = $_POST['fin'];
 
 $inicio = $primervalor;
 
-    echo '<table border=1px solid black width="100%">';
+    echo '<table border=2px solid black">';
 
     $Record = dbase_get_record_with_names ($db, $primervalor);
     echo '<tr><th>Contador Propio</th>';
 
-
+$cero = '';
         foreach ($Record as $key => $value){
 
             echo '<th>'.$key.'</th>';
 
 
         }
-
+echo strlen('');
     echo '</tr>';
 
     for ($primervalor; $primervalor <= $segundovalor; $primervalor++) {
@@ -27,8 +27,12 @@ $inicio = $primervalor;
         echo '<tr>';
         echo '<td>'.$primervalor.'</td>';
         foreach ($Record as $key => $value){
-
+            if ($value === "        "){
+                echo '<td>.</td>';
+            }
+            else {
                 echo '<td>'.$value.'</td>';
+            }
 
 
         }
@@ -40,12 +44,12 @@ $inicio = $primervalor;
 
 ?>
     <style type="text/css">
-        }
+        table {border-collapse: collapse;}
         tr:nth-child(odd) { background-color:#D2EAE8; }
 
         tr:nth-of-type(odd) { background-color:#FFFFFF; }
 
         tr:nth-of-type(even) { background-color:#D2EAE8; }
-        td {border: 1px solid ; border-color: black; text-align: center;}
+        td {text-align: center; vertical-align: middle;}
 
     </style>
