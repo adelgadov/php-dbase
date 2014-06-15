49968bd0893d6cac5ed8bf1afc3987ef32cd5176
Alvaro <alvaro_delgado_1994@hotmail.com>
2014-06-15 23:10:00 +0200

    Muestro las tres tablas

--- procesar.php
+++ procesar.php
@@ -1,91 +1,123 @@
 <?php
+
 ini_set('max_execution_time', 300);
 $db_ContHist = dbase_open ('Goldmine DB/ContHist.DBF', 0);
 $db_Contact1 = dbase_open ('Goldmine DB/Contact1.DBF', 0);
 $db_Contact2 = dbase_open ('Goldmine DB/Contact2.DBF', 0);
-$i = 1;
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+$accountno = array();
 
-$num_reg_Contact1 = dbase_numrecords ($db_Contact1);
 
 $primervalor = $_POST['inicio'];
 $segundovalor = $_POST['fin'];
 
+//CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST
+
+echo '<table style="vertical-align: top;"><td><table border=2px solid black>';
 
 
-echo '<table border=2px solid black">';
+$Record_ContHist = dbase_get_record_with_names ($db_ContHist, $primervalor);
+$Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
+echo '<tr><th>Contador Propio</th>';
+foreach ($Record_ContHist as $Key_ContHist => $Value_ContHist){
 
-if ($primervalor < $segundovalor) {
+    if ($Key_ContHist == 'ACTVCODE' or $Key_ContHist == 'ACCOUNTNO'){
+        echo '<th>'.$Key_ContHist.'</th>';
+    }
+}
+
+
+echo '</tr>';
+
+for ($primervalor; $primervalor <= $segundovalor; $primervalor++) {
     $Record_ContHist = dbase_get_record_with_names ($db_ContHist, $primervalor);
 
-    echo '<tr><th>Contador Propio</th>';
+
     foreach ($Record_ContHist as $Key_ContHist => $Value_ContHist){
+        if ($Value_ContHist == "120") {
+            echo '<td>'.$primervalor.'</td>';
+            $Accountno_ContHist = $Record_ContHist ["ACCOUNTNO"];
+            array_push($accountno, $Record_ContHist ["ACCOUNTNO"]);
 
-        if ($Key_ContHist == 'ACTVCODE' or $Key_ContHist == 'ACCOUNTNO'){
-            echo '<th>'.$Key_ContHist.'</th>';
-    }}
-
-    echo '</tr>';
-
-    for ($primervalor; $primervalor <= $segundovalor; $primervalor++) {
-        $Record_ContHist = dbase_get_record_with_names ($db_ContHist, $primervalor);
-
-
-        foreach ($Record_ContHist as $Key_ContHist => $Value_ContHist){
-            if ($Value_ContHist == "120") {
-                echo '<td>'.$primervalor.'</td>';
-                $Accountno_ContHist = $Record_ContHist ["ACCOUNTNO"];
-
-                         echo '<td>'.$Accountno_ContHist.'</td>';
-                        echo '<td>'.$Value_ContHist.'</td>';
-
-
-
-            }
+            echo '<td>'.$Accountno_ContHist.'</td>';
+            echo '<td>'.$Value_ContHist.'</td>';
         }
+
+
+
 
         echo '</tr>';
     }
 
-}else {
-    $Record_ContHist = dbase_get_record_with_names ($db_ContHist, $segundovalor);
-    echo '<tr>';
-    echo '<tr><th>Contador Propio</th>';
 
-    foreach ($Record_ContHist as $Key_ContHist => $Value_ContHist){
-
-            echo '<th>'.$Key_ContHist.'</th>';
+}
 
 
-        }
+echo '</table></td>';
 
-    echo '</tr>';
+//CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1
 
-    for ($primervalor; $primervalor >= $segundovalor; $primervalor--) {
-        $Record_ContHist = dbase_get_record_with_names ($db_ContHist, $primervalor);
+echo '<td><table border=2px solid black>';
+$Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
 
-        echo '<tr>';
-        foreach ($Record_ContHist as $Key_ContHist => $Value_ContHist){
-            if ($Value_ContHist == "120") {
-                echo '<td>'.$primervalor.'</td>';
+foreach ($Record_Contact1 as $Key_Contact1 => $Value_Contact1){
 
-                foreach ($Record_ContHist as $Key_ContHist => $Value_ContHist){
-                    echo '<td>'.$Value_ContHist.'</td>';
+    if ($Key_Contact1 == 'CONTACT' or $Key_Contact1 == 'ACCOUNTNO'){
+        echo '<th>'.$Key_Contact1.'</th>';
+    }
+}
 
+for ($i = 0; $i <= count($accountno); $i++) {
+    for ($num_reg_Contact1 = dbase_numrecords ($db_Contact1); $num_reg_Contact1 >= 1; $num_reg_Contact1--) {
+        $Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
 
-                }
+        foreach ($Record_Contact1 as $Key_Contact1 => $Value_Contact1){
 
-            }
+            if ($Value_Contact1 === $accountno[$i]) {
 
-            echo '</tr>';
+                echo '<td>'.$Value_Contact1.'</td>';
+                echo '<td>'.$Record_Contact1 ["CONTACT"].'</td>';
+
+            }echo '</tr>';
         }
 
     }
+}
+echo '</table></td>';
 
+//CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2
+
+echo '<td><table border=2px solid black>';
+$Record_Contact2 = dbase_get_record_with_names ($db_Contact2, $num_reg_Contact2);
+
+foreach ($Record_Contact2 as $Key_Contact2 => $Value_Contact2){
+
+    if ($Key_Contact2 == 'UPESTADO' or $Key_Contact2 == 'ACCOUNTNO'){
+        echo '<th>'.$Key_Contact2.'</th>';
+    }
+}
+
+for ($i = 0; $i <= count($accountno); $i++) {
+    for ($num_reg_Contact2 = dbase_numrecords ($db_Contact2); $num_reg_Contact2 >= 1; $num_reg_Contact2--) {
+        $Record_Contact2 = dbase_get_record_with_names ($db_Contact2, $num_reg_Contact2);
+
+        foreach ($Record_Contact2 as $Key_Contact2 => $Value_Contact2){
+
+            if ($Value_Contact2 === $accountno[$i]) {
+
+                echo '<td>'.$Value_Contact2.'</td>';
+                echo '<td>'.$Record_Contact2 ["UPESTADO"].'</td>';
+
+            }echo '</tr>';
+        }
 
     }
-echo '</table>';
+}
+echo '</table></td></table>';
 
 
+
+print_r($accountno);
 ?>
 <style type="text/css">
     table {border-collapse: collapse;}
