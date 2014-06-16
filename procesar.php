fa498237a4577dfc124ae94696f0ecb3938a0f25
Alvaro <alvaro_delgado_1994@hotmail.com>
2014-06-16 13:34:40 +0200

    Limpiado el código

--- procesar.php
+++ procesar.php
@@ -4,8 +4,11 @@ ini_set('max_execution_time', 1000000);
 $db_ContHist = dbase_open ('Goldmine DB/ContHist.DBF', 0);
 $db_Contact1 = dbase_open ('Goldmine DB/Contact1.DBF', 0);
 $db_Contact2 = dbase_open ('Goldmine DB/Contact2.DBF', 0);
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(0);
 $accountno = array();
+$finalizar_Contact1 = array();
+$finalizar_Contact2 = array();
+
 
 
 $primervalor = $_POST['inicio'];
@@ -13,112 +16,114 @@ $segundovalor = $_POST['fin'];
 
 //CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST
 
-echo '<table style="vertical-align: top;"><td><table border=2px solid black>';
+?><div style="position: absolute;width: 150%;">
+    <td>
+        <table style="position: absolute;border-collapse: collapse; border-spacing: 0; padding: 0; margin: 0">
+            <td style="vertical-align: top; border-collapse: collapse; border-spacing: 0; padding: 0">
+                <table border=2px solid black><?php
 
+                    $Record_ContHist = dbase_get_record_with_names ($db_ContHist, $primervalor);
+                    ?><tr><th>Contador Propio</th><?php
 
-$Record_ContHist = dbase_get_record_with_names ($db_ContHist, $primervalor);
-$Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
-echo '<tr><th>Contador Propio</th>';
-foreach ($Record_ContHist as $Key_ContHist => $Value_ContHist){
+                            ?><th>ACCOUNTNO</th><?php
+                            ?><th>ACTVCODE</th><?php
 
-    if ($Key_ContHist == 'ACTVCODE' or $Key_ContHist == 'ACCOUNTNO'){
-        echo '<th>'.$Key_ContHist.'</th>';
-    }
-}
+                            ?></tr><?php
 
-
-echo '</tr>';
-
-for ($primervalor; $primervalor <= $segundovalor; $primervalor++) {
-    $Record_ContHist = dbase_get_record_with_names ($db_ContHist, $primervalor);
-
-
-    foreach ($Record_ContHist as $Key_ContHist => $Value_ContHist){
-        if ($Value_ContHist == "120") {
-            echo '<td>'.$primervalor.'</td>';
-            $Accountno_ContHist = $Record_ContHist ["ACCOUNTNO"];
-            array_push($accountno, $Record_ContHist ["ACCOUNTNO"]);
-
-            echo '<td>'.$Accountno_ContHist.'</td>';
-            echo '<td>'.$Value_ContHist.'</td>';
-        }
+                            for ($primervalor; $primervalor <= $segundovalor; $primervalor++) {
+                                $Record_ContHist = dbase_get_record_with_names ($db_ContHist, $primervalor);
 
 
 
+                                    if ($Record_ContHist["ACTVCODE"] == "120") {
+                                        ?><td> <?php echo $primervalor ?></td>
 
-        echo '</tr>';
-    }
+                                    <?php
+                                        $Accountno_ContHist = $Record_ContHist ["ACCOUNTNO"];
+                                        array_push($accountno, $Record_ContHist ["ACCOUNTNO"]);
+
+                                    ?><td><?php echo $Accountno_ContHist ?></td>
+                                    <?php
+                                    ?><td><?php echo $Record_ContHist["ACTVCODE"] ?></td>
+
+                                    <?php
+                                    }
+                    ?></tr><?php
 
 
-}
+
+                            }
 
 
-echo '</table></td>';
-dbase_close($db_ContHist);
+                ?></table>
+            </td><?php
+                    dbase_close($db_ContHist);
+
 //CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1
 
-echo '<td><table border=2px solid black>';
-$Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
+            ?><td style="vertical-align: top; border-collapse: collapse; border-spacing: 0px; padding: 0px">
+                <table border=2px solid black><?php
+                     $Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
 
-foreach ($Record_Contact1 as $Key_Contact1 => $Value_Contact1){
+                    ?><th> CONTACT </th><?php
 
-    if ($Key_Contact1 == 'CONTACT' or $Key_Contact1 == 'ACCOUNTNO'){
-        echo '<th>'.$Key_Contact1.'</th>';
-    }
-}
+                    for ($i = 0; $i <= count($accountno); $i++) {
+                        for ($num_reg_Contact1 = dbase_numrecords ($db_Contact1); $Record_Contact1 ["ACCOUNTNO"] != $accountno [$i]; $num_reg_Contact1--) {
+                            $Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
 
-for ($i = 0; $i <= count($accountno); $i++) {
-    for ($num_reg_Contact1 = dbase_numrecords ($db_Contact1); $num_reg_Contact1 >= 1; $num_reg_Contact1--) {
-        $Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
 
-        foreach ($Record_Contact1 as $Key_Contact1 => $Value_Contact1){
 
-            if ($Value_Contact1 === $accountno[$i]) {
+                                if ($Record_Contact1["ACCOUNTNO"] === $accountno[$i]) {
 
-                echo '<td>'.$Value_Contact1.'</td>';
-                echo '<td>'.$Record_Contact1 ["CONTACT"].'</td>';
 
-            }echo '</tr>';
-        }
+                                    ?><td><?php echo $Record_Contact1 ["CONTACT"]?></td><?php
+                                    array_push($finalizar_Contact1, $Record_Contact1 ["ACCOUNTNO"]);
+                                }
+                            ?></tr><?php
+                            if (count($finalizar_Contact1) == count($accountno)) {
+                                break;
+                            }
 
-    }
-}
-echo '</table></td>';
-dbase_close($db_Contact1);
+
+                        }continue;
+                    }
+                ?></table>
+            </td><?php
+                    dbase_close($db_Contact1);
+
 //CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2
 
-echo '<td><table border=2px solid black>';
-$Record_Contact2 = dbase_get_record_with_names ($db_Contact2, $num_reg_Contact2);
+            ?><td style="vertical-align: top; border-collapse: collapse; border-spacing: 0px; padding: 0px">
+                <table border=2px solid black style="vertical-align: middle; text-align: center;"><?php
+                    $Record_Contact2 = dbase_get_record_with_names ($db_Contact2, $num_reg_Contact2);
 
-foreach ($Record_Contact2 as $Key_Contact2 => $Value_Contact2){
+                    ?> <th> PREVRESULT </th><?php
 
-    if ($Key_Contact2 == 'PREVRESULT' or $Key_Contact2 == 'ACCOUNTNO'){
-        echo '<th>'.$Key_Contact2.'</th>';
-    }
-}
+                    for ($i = 0; $i <= count($accountno); $i++) {
+                        for ($num_reg_Contact2 = dbase_numrecords ($db_Contact2);$Record_Contact2 ["ACCOUNTNO"] != $accountno [$i]; $num_reg_Contact2--) {
+                            $Record_Contact2 = dbase_get_record_with_names ($db_Contact2, $num_reg_Contact2);
 
-for ($i = 0; $i <= count($accountno); $i++) {
-    for ($num_reg_Contact2 = dbase_numrecords ($db_Contact2); $num_reg_Contact2 >= 1; $num_reg_Contact2--) {
-        $Record_Contact2 = dbase_get_record_with_names ($db_Contact2, $num_reg_Contact2);
 
-        foreach ($Record_Contact2 as $Key_Contact2 => $Value_Contact2){
+                            if ($Record_Contact2["ACCOUNTNO"] === $accountno[$i]) {
 
-            if ($Value_Contact2 === $accountno[$i]) {
+                                    ?><td><?php echo str_replace("                                                                 ", ".", $Record_Contact2 ["PREVRESULT"])?></td><?php
+                                    array_push($finalizar_Contact2, $Record_Contact2 ["ACCOUNTNO"]);
 
-                echo '<td>'.$Value_Contact2.'</td>';
-                echo '<td>'.$Record_Contact2 ["PREVRESULT"].'</td>';
+                            }
+                            ?></tr><?php
 
-            }echo '</tr>';
-        }
+                            if (count($finalizar_Contact2) == count($accountno)) {
+                                break;
+                            }
 
-    }
-}
-echo '</table></td></table>';
+                        }continue;
+                    }
+        ?></table></td></table></div><?php
 
-dbase_close($db_Contact2);
+                    dbase_close($db_Contact2);
 
-print_r($accountno);
-?>
+
+        ?>
 <style type="text/css">
     table {border-collapse: collapse;}
     tr:nth-child(odd) { background-color:#D2EAE8; }
@@ -126,6 +131,6 @@ print_r($accountno);
     tr:nth-of-type(odd) { background-color:#FFFFFF; }
 
     tr:nth-of-type(even) { background-color:#D2EAE8; }
-    td {text-align: center; vertical-align: middle;}
+    body {background-image: URL("http://boletin.esy.es/boletin/body_bg.png")}
 
 </style>
