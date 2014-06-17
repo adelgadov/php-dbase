ba224d606419b5855a4de004be0be8d4a011cf3c
Alvaro <alvaro_delgado_1994@hotmail.com>
2014-06-17 17:06:48 +0200

    Hace busquedas por fechas, introducimos la fecha en el siguiente formato: dd/mm/yyyy

--- index.php
+++ index.php
@@ -4,22 +4,32 @@
     <head>
 <meta charset="UTF-8">
     </head>
-    <table class="table_index">
-        <form method="post" action="procesar.php" target=”_blank”>
-            <tr>
-                <td>Mostrar registros de :</td>
-                <td><input type="text" name="inicio"></td>
-                <td>a</td>
-                <td><input type="text" name="fin"></td>
-                <td>(Máx.<?php $db = dbase_open ('Goldmine DB/ContHist.DBF', 0); $num_reg = dbase_numrecords ($db); echo $num_reg; ?>)</td>
-                <td><input type="submit" value="Enviar"></td>
-            </tr>
+        <body>
+            <table class="table_index">
+                <form method="post" action="procesar.php" target=”_blank”>
+                    <tr>
+                        <td></td>
+                        <td align="center"><font color="gray">dd/mm/yyyy</font></td>
+                        <td></td>
+                        <td align="center"><font color="gray">dd/mm/yyyy</font></td>
+                    </tr>
+                    <tr>
+                        <td>Mostrar registros de :</td>
+                        <td><input type="text" name="inicio"></td>
+                        <td>a</td>
+                        <td><input type="text" name="fin"</td>
+                        <td>(Máx.<?php $db = dbase_open ('Goldmine DB/ContHist.DBF', 0); $num_reg = dbase_numrecords ($db); echo $num_reg; ?>)</td>
+                        <td><input type="submit" value="Enviar"></td>
+                    </tr>
 
-        </form>
-    </table>
-    <body>
-
-
+                </form>
+            </table>
+        <style type="text/css">
+            body {
+                background-image: url("http://oi59.tinypic.com/2udysco.jpg");
+            }
+        </style>
     </body>
+
 </html>
 
--- procesar.php
+++ procesar.php
@@ -7,13 +7,21 @@ $db_Contact2 = dbase_open ('Goldmine DB/Contact2.DBF', 0);
 error_reporting(0);
 $accountno = array();
 $array_accountno = array();
+
 $finalizar_Contact1 = array();
 $finalizar_Contact2 = array();
 
+$valoruno = str_replace("/","", $_POST['inicio']);
+$valordos =str_replace("/","",$_POST['fin']);
+
+explode("/",$valoruno);
+explode("/", $valordos);
+
+$primervalor = $valoruno[4].$valoruno[5].$valoruno[6].$valoruno[7].$valoruno[2].$valoruno[3].$valoruno[0].$valoruno[1];
+$segundovalor = $valordos[4].$valordos[5].$valordos[6].$valordos[7].$valordos[2].$valordos[3].$valordos[0].$valordos[1];
 
 
-$primervalor = $_POST['inicio'];
-$segundovalor = $_POST['fin'];
+
 
 //CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST CONTHIST
 
@@ -21,44 +29,41 @@ $segundovalor = $_POST['fin'];
     <td>
         <table style="position: absolute;border-collapse: collapse; border-spacing: 0; padding: 0; margin: 0">
             <td style="vertical-align: top; border-collapse: collapse; border-spacing: 0; padding: 0">
-                <table border=2px solid black><?php
+                <table>  <?php
 
 
-                    ?><tr><th>Contador Propio</th><?php
+                    ?>  <tr><th style="border: 1px solid black">Contador Propio</th>  <?php
 
-                            ?><th>ACCOUNTNO</th><?php
-                            ?><th>ACTVCODE</th><?php
-                            ?><th>ONDATE</th><?php
+                            ?>  <th style="border: 1px solid black">ACCOUNTNO</th>  <?php
+                            ?>  <th style="border: 1px solid black">ACTVCODE</th>   <?php
+                            ?>  <th style="border: 1px solid black">ONDATE</th> <?php
 
-                            ?></tr><?php
+                            ?>  </tr>   <?php
 
                             for ($i, $num_reg_ContHist = dbase_numrecords ($db_ContHist); $i <= $num_reg_ContHist; $i++) {
                                 $Record_ContHist = dbase_get_record_with_names ($db_ContHist, $i);
 
+                                if ($Record_ContHist["ACTVCODE"] == "120") {
+                                        if ($Record_ContHist["ONDATE"] >= $primervalor && $Record_ContHist ["ONDATE"] <= $segundovalor) {
+                                            ?>  <tr><td style="border: 1px solid black">    <?php echo $primervalor ?></td>
 
-                            if ($Record_ContHist["ONDATE"] >= $primervalor && $Record_ContHist ["ONDATE"] <= $segundovalor) {
-                                    if ($Record_ContHist["ACTVCODE"] == "120") {
-                                        ?><td> <?php echo $primervalor ?></td>
+                                        <?php
+                                            $Accountno_ContHist = $Record_ContHist ["ACCOUNTNO"];
+                                            array_push($array_accountno, $Record_ContHist ["ACCOUNTNO"]);
+                                            explode("-",$Record_ContHist["ONDATE"]);
+
+                                        ?>  <td style="border: 1px solid black">    <?php echo $Accountno_ContHist ?>   </td>
+                                        <?php
+                                        ?>  <td style="border: 1px solid black">    <?php echo $Record_ContHist["ACTVCODE"] ?>  </td>
+                                        <td style="border: 1px solid black">    <?php echo $Record_ContHist["ONDATE"][6].$Record_ContHist["ONDATE"][7]."/".$Record_ContHist["ONDATE"][4].$Record_ContHist["ONDATE"][5]."/".
+                                            $Record_ContHist["ONDATE"][0].$Record_ContHist["ONDATE"][1].$Record_ContHist["ONDATE"][2].$Record_ContHist["ONDATE"][3] ?>  </td></tr>
+
 
                                     <?php
-                                        $Accountno_ContHist = $Record_ContHist ["ACCOUNTNO"];
-                                        array_push($array_accountno, $Record_ContHist ["ACCOUNTNO"]);
-                                        explode("-",$Record_ContHist["ONDATE"]);
+                                        }
+                    ?>  </tr>   <?php
 
-                                    ?><td><?php echo $Accountno_ContHist ?></td>
-                                    <?php
-                                    ?><td><?php echo $Record_ContHist["ACTVCODE"] ?></td>
-                                    <td><?php echo $Record_ContHist["ONDATE"][6].$Record_ContHist["ONDATE"][7]."/".$Record_ContHist["ONDATE"][4].$Record_ContHist["ONDATE"][5]."/".
-                                        $Record_ContHist["ONDATE"][0].$Record_ContHist["ONDATE"][1].$Record_ContHist["ONDATE"][2].$Record_ContHist["ONDATE"][3] ?></td>
-
-
-                                    <?php
-                                    }
-                    ?></tr><?php
-
-
-
-                            }
+                                 }
                             }
 
                 ?></table>
@@ -67,43 +72,47 @@ $segundovalor = $_POST['fin'];
 $accountno = array_unique($array_accountno, $SORT_STRING = SORT_REGULAR);
 //CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1 CONTACT1
 
-            ?><td style="vertical-align: top; border-collapse: collapse; border-spacing: 0px; padding: 0px">
-                <table border=2px solid black><?php
+         ?>     <td style="vertical-align: top; border-collapse: collapse; border-spacing: 0; padding: 0">
+                    <table>  <?php
                      $Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
 
-                    ?><th> CONTACT </th><?php
+                    ?>  <tr><th style="border: 1px solid black"> CONTACT </th></tr><?php
 
-                    for ($i = 0; $i <= count($accountno); $i++) {
-                        for ($num_reg_Contact1 = dbase_numrecords ($db_Contact1); $Record_Contact1 ["ACCOUNTNO"] != $accountno [$i]; $num_reg_Contact1--) {
-                            $Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
-
+                        for ($i = 0; $i <= count($accountno); $i++) {
+                            for ($num_reg_Contact1 = dbase_numrecords ($db_Contact1);$Record_Contact1 ["ACCOUNTNO"] != $accountno [$i]; $num_reg_Contact1--) {
+                                $Record_Contact1 = dbase_get_record_with_names ($db_Contact1, $num_reg_Contact1);
 
 
                                 if ($Record_Contact1["ACCOUNTNO"] === $accountno[$i]) {
-
-
-                                    ?><td><?php echo str_replace("                                                                 "," ", ".", $Record_Contact1 ["CONTACT"])?></td><?php
+                                    if (strlen($Record_Contact1["CONTACT"]) == 0){
+                                        ?>  <tr><td style="border: 1px solid black">.</td></tr>  <?php
+                                    }
+                                    else {
+                                        ?>  <tr><td style="border: 1px solid black">    <?php   echo $Record_Contact1["CONTACT"] ?>      </td></tr>     <?php
+                                    }
                                     array_push($finalizar_Contact1, $Record_Contact1 ["ACCOUNTNO"]);
+
                                 }
-                            ?></tr><?php
-                            if (count($finalizar_Contact1) == count($accountno)) {
-                                break;
-                            }
+                                ?>  </tr>   <?php
 
+                                if (count($finalizar_Contact1) == count($accountno)) {
+                                    break;
+                                }
 
-                        }continue;
+                            }continue;
+
                     }
-                ?></table>
-            </td><?php
+                ?>  </table>
+            </td>   <?php
                     dbase_close($db_Contact1);
 
 //CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2 CONTACT2
 
-            ?><td style="vertical-align: top; border-collapse: collapse; border-spacing: 0px; padding: 0px">
-                <table border=2px solid black><?php
+            ?>  <td style="vertical-align: top; border-collapse: collapse; border-spacing: 0; padding: 0">
+                    <table>  <?php
                     $Record_Contact2 = dbase_get_record_with_names ($db_Contact2, $num_reg_Contact2);
 
-                    ?> <th> PREVRESULT </th><?php
+                    ?>  <th style="border: 1px solid black"> PREVRESULT </th>   <?php
 
                     for ($i = 0; $i <= count($accountno); $i++) {
                         for ($num_reg_Contact2 = dbase_numrecords ($db_Contact2);$Record_Contact2 ["ACCOUNTNO"] != $accountno [$i]; $num_reg_Contact2--) {
@@ -111,12 +120,16 @@ $accountno = array_unique($array_accountno, $SORT_STRING = SORT_REGULAR);
 
 
                             if ($Record_Contact2["ACCOUNTNO"] === $accountno[$i]) {
-
-                                    ?><td><?php echo str_replace("                                                                 ", ".", $Record_Contact2 ["PREVRESULT"])?></td><?php
-                                    array_push($finalizar_Contact2, $Record_Contact2 ["ACCOUNTNO"]);
+                                if (strlen($Record_Contact2["PREVRESULT"]) == 0){
+                                    ?>  <tr><td style="border: 1px solid black">.</td></tr>  <?php
+                                    }
+                                else {
+                                    ?>  <tr><td style="border: 1px solid black">    <?php   echo $Record_Contact2["PREVRESULT"] ?>      </td></tr>     <?php
+                                }
+                                  array_push($finalizar_Contact2, $Record_Contact2 ["ACCOUNTNO"]);
 
                             }
-                            ?></tr><?php
+                            ?>  </tr>   <?php
 
                             if (count($finalizar_Contact2) == count($accountno)) {
                                 break;
@@ -124,12 +137,11 @@ $accountno = array_unique($array_accountno, $SORT_STRING = SORT_REGULAR);
 
                         }continue;
                     }
-        ?></table></td></table></div><?php
+        ?>  </table></td></table></div>     <?php
 
                     dbase_close($db_Contact2);
 
-
-        ?>
+?>
 <style type="text/css">
     table {border-collapse: collapse;}
     tr:nth-child(odd) { background-color:#D2EAE8; }
@@ -138,5 +150,6 @@ $accountno = array_unique($array_accountno, $SORT_STRING = SORT_REGULAR);
 
     tr:nth-of-type(even) { background-color:#D2EAE8; }
     body {background-image: URL("http://boletin.esy.es/boletin/body_bg.png")}
+
 
 </style>
